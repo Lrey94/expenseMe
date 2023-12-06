7bf877b363d9340b5ef931b702e4b8fd7fafc047
Lrey94 <reynoldslawrence21@yahoo.co.uk>
2023-12-06 08:46:11 +0000

    Initial Commit

---
+++ expenseMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  expenseMe
+//
+//  Created by Lawrence Reynolds on 06/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ expenseMe/expenseMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseMeApp.swift
+//  expenseMe
+//
+//  Created by Lawrence Reynolds on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct expenseMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
